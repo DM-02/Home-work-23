6f63347985e6db88b5cc8aabfcc2cceb2df241b9
DM-02 <kid79ka@gmail.com>
2024-03-13 18:51:14 +0500

    Initial Commit

---
+++ algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by Дмитрий Сергеевич on 13.03.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
